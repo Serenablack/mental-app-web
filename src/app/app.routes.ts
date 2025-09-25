c21fa53ae0c23d3c7b839156f2a4337251aa164a
Serenablack <sunsetsin1.sy@gmail.com>
2025-09-25 09:47:30 -0500

    feat: adds route guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,39 +1,51 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { MoodEntryFormComponent } from './components/mood-entry-form/mood-entry-form.component';
-// import { HistoryComponent } from './components/history/history.component';
-import { AuthComponent } from './components/auth/auth.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'mood-entry', component: MoodEntryFormComponent },
-  { path: 'mood-entry/:id', component: MoodEntryFormComponent },
-  // { path: 'history', component: HistoryComponent },
-  { path: 'auth', component: AuthComponent },
-  { path: '**', redirectTo: '/dashboard' },
+  { path: '', redirectTo: '/auth', pathMatch: 'full' },
   {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full',
+    path: 'auth',
+    loadComponent: () =>
+      import('./components/auth/auth.component').then((m) => m.AuthComponent),
   },
-
-  // {
-  //   path: 'history',
-  //   loadChildren: () => import('./features/history/history.module').then(m => m.HistoryModule),
-  //   canActivate: [AuthGuard]
-  // },
-  // {
-  //   path: 'analytics',
-  //   loadChildren: () => import('./features/analytics/analytics.module').then(m => m.AnalyticsModule),
-  //   canActivate: [AuthGuard]
-  // },
-  // {
-  //   path: 'error',
-  //   loadChildren: () => import('./features/error/error.module').then(m => m.ErrorModule)
-  // },
-  // {
-  //   path: '**',
-  //   redirectTo: '/error'
-  // }
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./components/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'mood-entry',
+    loadComponent: () =>
+      import('./components/mood-entry-form/mood-entry-form.component').then(
+        (m) => m.MoodEntryFormComponent
+      ),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'mood-entry/:id',
+    loadComponent: () =>
+      import('./components/mood-entry-form/mood-entry-form.component').then(
+        (m) => m.MoodEntryFormComponent
+      ),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'history',
+    loadComponent: () =>
+      import('./components/history/history.component').then(
+        (m) => m.HistoryComponent
+      ),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'error',
+    loadComponent: () =>
+      import('./components/error-page/error-page.component').then(
+        (m) => m.ErrorPageComponent
+      ),
+  },
+  { path: '**', redirectTo: '/error' },
 ];
